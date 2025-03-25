36dd7f4bd9904c9ab4ace246d4f0fe4bee7d3a38
programinggiff <200276851+programinggiff@users.noreply.github.com>
2025-03-25 16:31:10 +0000

    atribuicao de resultados

---
+++ .vscode/first2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+main (){
+/*
+Atribuição Simples (=)
+Atribuição com Soma (+=)
+Atribuição com Subração (-=)
+Atribuiçã0 com Multiplicação (*=)
+Atribuição com Divisão(/=)
+*/
+int numero1 = 10, numero2, resultado;
+
+resultado = 10;
+printf("Resultado: %d\n", resultado);
+
+resultado += 20;
+printf("Resultado: %d\n", resultado);
+
+resultado -= numero1;
+printf("Resultado: %d\n", resultado);
+
+resultado *= 5;
+printf("Resultado %d\n", resultado);
+
+resultado /= 2;
+printf("Resultado: %d\n", resultado);
+
+
+
+    
+}

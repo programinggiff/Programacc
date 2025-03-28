cf551bec0eb15b011ec8a101f8d74eb503d95854
programinggiff <200276851+programinggiff@users.noreply.github.com>
2025-03-28 00:15:46 +0000

    commit float

---
+++ .vscode/first4/first4_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){ 
+    float x = 5.5;
+    float y = 2.2;
+    float soma = x + y;
+    float diferenca = x - y;
+    float produto = x * y;
+    float quoeficiente = x / y; //Divisão de ponto flutante
+
+
+
+
+}    
